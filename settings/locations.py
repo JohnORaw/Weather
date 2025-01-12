b028eaf0de0849caa69f2ad78bc33fbc9b3c4464
John ORaw <49677015+JohnORaw@users.noreply.github.com>
2025-01-12 12:03:39 +0000

    Create locations.py

---
+++ settings/locations.py
@@ -0,0 +1,7 @@
+# Latitude and Longitude
+locations = {
+    'Malin Head': ['55.381', '-7.372'],
+    'Buncrana': ['55.136', '-7.457'],
+    'Fanad Head': ['55.277', '-7.632'],
+    'Letterkenny': ['54.956', '-7.734'],
+}
